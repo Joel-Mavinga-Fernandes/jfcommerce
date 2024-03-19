d069a3ba037094f3119e88dcdc652531cc7090df
Joel Fernandes <joelfernandes35@hotmail.com>
2024-03-19 13:24:30 -0300

    create entities

---
+++ src/main/java/com/jstore/jfcommerce/JfcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.jstore.jfcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfcommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jstore/jfcommerce/entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.jstore.jfcommerce.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED,CANCELED;
+}
